c6c2e82d119c21ad541aea8584d0d41acaaa0dc5
Enti-Pol <pol.garriga@enti.cat>
2021-05-10 10:55:05 +0200

    Viento

---
+++ Assets/Scripts/windPush.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windPush : MonoBehaviour
+{
+    private Rigidbody2D rgb2;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.TryGetComponent<Rigidbody2D>(out rgb2);
+        rgb2.AddForce(new Vector2(10f, 0));
+    }
+}
